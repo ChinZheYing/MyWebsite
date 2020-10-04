39029ef94646aad39fb45fde6bf6e7baaacc3706
Zhe Ying Ying <chinzheying@gmail.com>
2020-10-04 11:47:37 +0000

    Added rename tool

--- programming/projects.js
+++ programming/projects.js
@@ -1,5 +1,7 @@
-populateItems("cardview-parent","Dodge game","<p>Java</p><p>This is a simple single player game created in Java. You just control a plane to dodge falling balls. Score is shown in console using print()</p>", "https://github.com/ChinZheYing/Dodge-Game","Images/dodge.jpg","Source Code");
+populateItems("cardview-parent","Dodge Game","<p>Java</p><p>This is a simple single player game created in Java. You just control a plane to dodge falling balls. Score is shown in console using print()</p>", "https://github.com/ChinZheYing/Dodge-Game","Images/dodge.jpg","Source Code");
 
 populateItems("cardview-parent","My Website","<p>Html, CSS, JavaScript</p><p>This is a simple website I made to post my projects, awards and random stuff", "https://github.com/ChinZheYing/MyWebsite","Images/mywebsite.jpg","Source Code");
 
-populateItems("cardview-parent","Pop game","<p>Processing</p><p>Simple game where you click balls of random sizes that move at random speed and angles", "https://github.com/ChinZheYing/Pop-Game","Images/popgame.jpg","Source Code");
+populateItems("cardview-parent","Pop Game","<p>Processing</p><p>Simple game where you click balls of random sizes that move at random speed and angles", "https://github.com/ChinZheYing/Pop-Game","Images/popgame.jpg","Source Code");
+
+populateItems("cardview-parent","Rename Tool","<p>Java</p><p>This is a tool that allows you to rename a huge number of files all at once</p><p>For example renaming photo from cameras which has weird names</p>", "https://github.com/ChinZheYing/Rename-Tool","Images/renametool.jpg","Source Code");
