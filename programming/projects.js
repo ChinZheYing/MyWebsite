7af4d8ea3b5fd21bcb0f1ada589ffedd00a35e3a
Zhe Ying Ying <chinzheying@gmail.com>
2020-10-06 07:49:36 +0000

    V1.1.02

--- programming/projects.js
+++ programming/projects.js
@@ -1,13 +1,13 @@
+populateItems("cardview-parent","Sudoku Solver","<p>Python</p><p>Solves a Sudoku you enter</p><p>Reference: None</p>", "https://github.com/ChinZheYing/Sudoku-Solver","Images/sudokusolver.jpg","Source Code");
+
+populateItems("cardview-parent","A* Path Finder","<p>Python</p><p>Draw a maze and let the program do the solving</p><p>Reference: https://www.youtube.com/watch?v=JtiK0DOeI4A&t=1264s</p>", "https://github.com/ChinZheYing/PathFinder","Images/pathfinder.jpg","Source Code");
+
 populateItems("cardview-parent","Dodge Game","<p>Java</p><p>This is a simple single player game created in Java. You just control a plane to dodge falling balls. Score is shown in console using print()</p><p>Reference: None</p>", "https://github.com/ChinZheYing/Dodge-Game","Images/dodge.jpg","Source Code");
+
+populateItems("cardview-parent","Bulk File Rename","<p>Java</p><p>This is a tool that allows you to rename a huge number of files all at once</p><p>For example renaming photo from cameras which has weird names</p><p>Reference: None</p>", "https://github.com/ChinZheYing/Rename-Tool","Images/renametool.jpg","Source Code");
 
 populateItems("cardview-parent","My Website","<p>Html, CSS, JavaScript</p><p>This is a simple website I made to post my projects, awards and random stuff<p>Reference: https://www.w3schools.com/howto/howto_make_a_website.asp</p>", "https://github.com/ChinZheYing/MyWebsite","Images/mywebsite.jpg","Source Code");
 
 populateItems("cardview-parent","Pop Game","<p>Processing</p><p>Simple game where you click balls of random sizes that move at random speed and angles<p>Reference: A YouTube video I watched too long ago to remember</p.", "https://github.com/ChinZheYing/Pop-Game","Images/popgame.jpg","Source Code");
 
-populateItems("cardview-parent","Bulk File Rename","<p>Java</p><p>This is a tool that allows you to rename a huge number of files all at once</p><p>For example renaming photo from cameras which has weird names</p><p>Reference: None</p>", "https://github.com/ChinZheYing/Rename-Tool","Images/renametool.jpg","Source Code");
-
 populateItems("cardview-parent","Spaceapp","<p>Html, CSS, JavaScript</p><p>Planet at our fingertip. Just redirect to Nasa Earthdata :)</p><p>Reference: https://www.w3schools.com/howto/howto_make_a_website.asp</p>", "https://github.com/ChinZheYing/spaceapps","Images/spaceapp.jpg","Source Code");
-
-populateItems("cardview-parent","A* Path Finder","<p>Python</p><p>Draw a maze and let the program do the solving</p><p>Reference: https://www.youtube.com/watch?v=JtiK0DOeI4A&t=1264s</p>", "https://github.com/ChinZheYing/PathFinder","Images/pathfinder.jpg","Source Code");
-
-populateItems("cardview-parent","Sudoku Solver","<p>Python</p><p>Solves a Sudoku you enter</p><p>Reference: None</p>", "https://github.com/ChinZheYing/Sudoku-Solver","Images/sudokusolver.jpg","Source Code");
