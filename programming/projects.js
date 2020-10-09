a8f9ced5aca49478d7ec1603e5ba40a083d4fe2e
Zhe Ying Ying <chinzheying@gmail.com>
2020-10-09 13:15:30 +0000

    V1.1.04 Tic Tac Toe

--- programming/projects.js
+++ programming/projects.js
@@ -6,6 +6,8 @@ populateItems("cardview-parent","Dodge Game","<p>Java</p><p>This is a simple sin
 
 populateItems("cardview-parent","Bulk File Rename","<p>Java</p><p>This is a tool that allows you to rename a huge number of files all at once</p><p>For example renaming photo from cameras which has weird names</p><p>Reference: None</p>", "https://github.com/ChinZheYing/Rename-Tool","Images/renametool.jpg","Source Code");
 
+populateItems("cardview-parent","Tic Tac Toe","<p>Python</p><p>Just a game of Tic Tac Toe. Currently only pvp. Single player coming soon</p><p>Reference: None</p>", "https://github.com/ChinZheYing/Tic-Tac-Toe","Images/tictactoe.jpg","Source Code");
+
 populateItems("cardview-parent","My Website","<p>Html, CSS, JavaScript</p><p>This is a simple website I made to post my projects, awards and random stuff<p>Reference: https://www.w3schools.com/howto/howto_make_a_website.asp</p>", "https://github.com/ChinZheYing/MyWebsite","Images/mywebsite.jpg","Source Code");
 
 populateItems("cardview-parent","Pop Game","<p>Processing</p><p>Simple game where you click balls of random sizes that move at random speed and angles<p>Reference: A YouTube video I watched too long ago to remember</p.", "https://github.com/ChinZheYing/Pop-Game","Images/popgame.jpg","Source Code");
