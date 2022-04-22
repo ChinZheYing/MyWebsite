bbf9fe6ba8a82490fca9f27b91a470747540bb28
ChinZheYing <chinzheying@gmail.com>
2022-04-22 11:57:13 +0800

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-populateItems("cardview-parent","Programming Projects","Programming projects written in Java, Processing, Python, JavaScript, HTML and CSS", "programming/projects.html","programming/Images/programming.jpg","EXPLORE!");
-
-populateItems("cardview-parent","Awards","Awards I've gotten from 2016 till present", "awards/awards.html","awards/awards.jpg","EXPLORE!");
-
